62daec3c750b144054ef2ec71b981c5885626b77
Genncho <mart.e.genchev@gmail.com>
2025-05-19 20:09:49 +0200

    initial commit of whole table

---
+++ Frequency domain/FrequencyDomain.m
@@ -0,0 +1,146 @@
+%  ==========================================================================
+%  | Instructions to test individual exercise:                              |
+%  | Add a return at the end of the exercise code, so it stops executing    |
+%  |    further code                                                        |
+%  ==========================================================================
+
+% ex1
+clear all;
+close all;
+clc;
+image = zeros(200, 200, "uint8");
+x_start = 50;
+y_start = 50;
+width = 100;
+height = 100;
+
+image(y_start:y_start+height-1, x_start:x_start+width-1) = 255;
+figure; imshow(image, []);
+
+fspec = fft2(image);
+figure; imshow(abs(fspec), []);
+
+fspec_shifted = fftshift(fspec);
+figure; imshow(abs(fspec_shifted), []);
+
+magnitude_spectrum = log(1 + abs(fspec_shifted));
+figure; imshow(abs(magnitude_spectrum), []);
+
+% ex2
+clear all;
+close all;
+clc;
+
+snapshot = imread("files\mri_snapshot.jpg");
+snapshot = rgb2gray(snapshot);
+figure; imshow(snapshot, []);
+
+[M,N] = size(snapshot);
+freqSpec = fftshift(fft2(snapshot));
+[u, v] = meshgrid(-floor(N/2):ceil(N/2)-1, -floor(M/2):ceil(M/2)-1);
+D = sqrt(u.^2 + v.^2);
+D_max = max(D(:));
+cutoff_percent = 0.1;
+D0 = cutoff_percent * D_max;
+H = double(D <= D0);
+figure; imshow(H, []);
+
+figure; mesh(u, v, H);
+G = H .* freqSpec;
+img_filtered = real(ifft2(ifftshift(G)));
+figure; imshow(uint8(img_filtered));
+
+% ex3
+clear all;
+close all;
+clc;
+
+snapshot = imread("files\mri_snapshot.jpg");
+snapshot = rgb2gray(snapshot);
+figure; imshow(snapshot, []);
+
+[M,N] = size(snapshot);
+freqSpec = fftshift(fft2(snapshot));
+[u, v] = meshgrid(-floor(N/2):ceil(N/2)-1, -floor(M/2):ceil(M/2)-1);
+D = sqrt(u.^2 + v.^2);
+D_max = max(D(:));
+cutoff_percent = 0.5;
+D0 = cutoff_percent * D_max;
+H = 1 - double(D <= D0);
+figure; imshow(H, []);
+
+figure; mesh(u, v, H);
+
+G = H .* freqSpec;
+img_filtered = real(ifft2(ifftshift(G)));
+figure; imshow(img_filtered, []);
+
+
+
+return;
+
+%ex 4 
+clear all;
+close all;
+clc;
+
+% Load and preprocess image
+snapshot = imread("files\mri_snapshot.jpg");
+snapshot = rgb2gray(snapshot);
+[M, N] = size(snapshot);
+freqSpec = fftshift(fft2(snapshot));
+[u, v] = meshgrid(-floor(N/2):ceil(N/2)-1, -floor(M/2):ceil(M/2)-1);
+D = sqrt(u.^2 + v.^2);
+D_max = max(D(:));
+
+cutoff_vals = [0.01, 0.05, 0.5];
+figure('Name','Task 4: Lowpass Filtering','NumberTitle','off');
+
+for i = 1:length(cutoff_vals)
+    D0 = cutoff_vals(i) * D_max;
+    H = double(D <= D0);
+    G = H .* freqSpec;
+    img_filtered = real(ifft2(ifftshift(G)));
+    
+    subplot(2, 3, i);
+    imshow(H, []);
+    title(['LPF H, D0 = ' num2str(cutoff_vals(i))]);
+
+    subplot(2, 3, 3 + i);
+    imshow(uint8(img_filtered), []);
+    title(['LPF Img, D0 = ' num2str(cutoff_vals(i))]);
+end
+
+%ex 5 
+clear all;
+close all;
+clc;
+
+% Load and preprocess image
+snapshot = imread("files\mri_snapshot.jpg");
+snapshot = rgb2gray(snapshot);
+[M, N] = size(snapshot);
+freqSpec = fftshift(fft2(snapshot));
+[u, v] = meshgrid(-floor(N/2):ceil(N/2)-1, -floor(M/2):ceil(M/2)-1);
+D = sqrt(u.^2 + v.^2);
+D_max = max(D(:));
+
+cutoff_vals = [0.01, 0.05, 0.5];
+figure('Name','Task 5: Highpass Filtering','NumberTitle','off');
+
+for i = 1:length(cutoff_vals)
+    D0 = cutoff_vals(i) * D_max;
+    H = 1 - double(D <= D0);
+    G = H .* freqSpec;
+    img_filtered = real(ifft2(ifftshift(G)));
+    
+    subplot(2, 3, i);
+    imshow(H, []);
+    title(['HPF H, D0 = ' num2str(cutoff_vals(i))]);
+
+    subplot(2, 3, 3 + i );
+    imshow(uint8(img_filtered), []);
+    title(['HPF Img, D0 = ' num2str(cutoff_vals(i))]);
+end
+
+
